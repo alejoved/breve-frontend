659a84c6f032ee3cba67e531994e8976f8253df4
alejandro <joaaguir@bancolombia.com.co>
2025-11-10 13:28:07 -0500

    Change businessName

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ import { WithdrawalsViewComponent } from './app/b2b/withdrawals-view/withdrawals
 export class App {}
 
 const routes = [
-  { path: 'flow/:businessName', component: WelcomeComponent },
+  { path: 'subscription/:businessName', component: WelcomeComponent },
   { path: 'contact', component: ContactComponent },
   { path: 'plans', component: PlansComponent },
   { path: 'additional-info', component: AdditionalInfoComponent },
