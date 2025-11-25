6719ccbc2c270f5c39d27e52dcf36376cd0d50b7
alejandro <joaaguir@bancolombia.com.co>
2025-11-25 17:43:04 -0500

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     host : "http://localhost:5000",
-    username: 'admin',
-    password: 'admin'
+    username: 'dev',
+    password: 'dev'
 };
